fac088141e55630b6efdbbcf80bb31ce7447180c
tarikul-islam <tariikul12@gmail.com>
2023-05-09 17:02:09 +0600

    data update

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors')
-const port = 5000;
+const port = process.env.PORT || 5000;
 const dotenv = require("dotenv")
 
 app.use(cors())
 dotenv.config()
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_CHOCOLATE}:${process.env.DB_PASS}@cluster0.nlw4swl.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
@@ -25,12 +26,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
